134ce3746aab5ee29ebb4d929786d1cc215e5a58
Veronika Kurinna <kurinna.dev@gmail.com>
2023-05-05 16:41:13 +0300

    Add tests

--- src/Components/Sorting.js
+++ src/Components/Sorting.js
@@ -27,12 +27,18 @@ export const bubbleSort = async (numbers, onSwap, time) => {
     let array = numbers;
 
     for (let i = 0; i < array.length; i++) {
+        let elementsSwapped = false;
         for (let j = 0; j < array.length - 1; j++) {
             if (array[j] > array[j + 1]) {
                 await delay(time);
                 swap(array, j, j + 1)
                 onSwap(array);
+                elementsSwapped = true;
             }
+        }
+        let isArraySorted = elementsSwapped === false;
+        if (isArraySorted) {
+            break;
         }
     }
 }
