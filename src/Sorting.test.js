d5d3d18c69e311543d65d43a84f9b1c9282ba9d2
Veronika Kurinna <kurinna.dev@gmail.com>
2023-05-10 22:35:03 +0300

    Add test merge exception

--- src/Sorting.test.js
+++ src/Sorting.test.js
@@ -4,226 +4,226 @@ import { selectionSort } from './Components/Sorting';
 import { quickSort } from './Components/Sorting';
 import { mergeSort } from './Components/Sorting';
 
-// describe(
-//     'Test Bubble sort',
-//     () => {
-//         const testCases = [
-//             {
-//                 array: [6, 9, 2, -8, 4],
-//                 expected: [-8, 2, 4, 6, 9],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 5, 3, 4 ],
-//                 expected: [1, 2, 3, 4, 5],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 3, 4],
-//                 expected: [1, 2, 3, 4],
-//                 time: 1
-//             },
-//             {
-//                 array: [3],
-//                 expected: [3],
-//                 time: 1
-//             },            {
-//                 array: [ 3, 7, 2, 5, 4, 4 ],
-//                 expected: [ 2, 3, 4, 4, 5, 7],
-//                 time: 1
-//             }
-//         ];
-//         testCases.forEach(test => {
-//             it(
-//                 `Array: ${test.array}, expected: ${test.expected}`,
-//                 async () => {
-//                     let onSwap = () => {};
-//                     await bubbleSort(test.array, onSwap, test.time);
-//                     expect(test.array).toEqual(test.expected);
-//                 }
-//             );
-//         });
-//     }
-// )
+describe(
+    'Test Bubble sort',
+    () => {
+        const testCases = [
+            {
+                array: [6, 9, 2, -8, 4],
+                expected: [-8, 2, 4, 6, 9],
+                time: 1
+            },
+            {
+                array: [1, 2, 5, 3, 4 ],
+                expected: [1, 2, 3, 4, 5],
+                time: 1
+            },
+            {
+                array: [1, 2, 3, 4],
+                expected: [1, 2, 3, 4],
+                time: 1
+            },
+            {
+                array: [3],
+                expected: [3],
+                time: 1
+            },            {
+                array: [ 3, 7, 2, 5, 4, 4 ],
+                expected: [ 2, 3, 4, 4, 5, 7],
+                time: 1
+            }
+        ];
+        testCases.forEach(test => {
+            it(
+                `Array: ${test.array}, expected: ${test.expected}`,
+                async () => {
+                    let onSwap = () => {};
+                    await bubbleSort(test.array, onSwap, test.time);
+                    expect(test.array).toEqual(test.expected);
+                }
+            );
+        });
+    }
+)
 
-// test(
-//     'Test Bubble sort exception', 
-//     async() => {
-//         let array = [];
-//         let onSwap = () => {};
-//         try{
-//             await bubbleSort(array, onSwap, 1);
-//         } catch(e){
-//             expect(e.message).toBe('Array is empty');
-//         }
-//     }
-// );
+test(
+    'Test Bubble sort exception', 
+    async() => {
+        let array = [];
+        let onSwap = () => {};
+        try{
+            await bubbleSort(array, onSwap, 1);
+        } catch(e){
+            expect(e.message).toBe('Array is empty');
+        }
+    }
+);
 
-// describe(
-//     'Test Insertion Sort',
-//     () => {
-//         const testCases = [
-//             {
-//                 array: [6, 9, 2, -8, 4],
-//                 expected: [-8, 2, 4, 6, 9],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 5, 3, 4 ],
-//                 expected: [1, 2, 3, 4, 5],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 3, 4],
-//                 expected: [1, 2, 3, 4],
-//                 time: 1
-//             },
-//             {
-//                 array: [3],
-//                 expected: [3],
-//                 time: 1
-//             },            {
-//                 array: [ 3, 7, 2, 5, 4, 4 ],
-//                 expected: [ 2, 3, 4, 4, 5, 7],
-//                 time: 1
-//             }
-//         ];
-//         testCases.forEach(test => {
-//             it(
-//                 `Array: ${test.array}, expected: ${test.expected}`,
-//                 async () => {
-//                     let onSwap = () => {};
-//                     await insertionSort(test.array, onSwap, test.time);
-//                     expect(test.array).toEqual(test.expected);
-//                 }
-//             );
-//         });
-//     }
-// )
+describe(
+    'Test Insertion Sort',
+    () => {
+        const testCases = [
+            {
+                array: [6, 9, 2, -8, 4],
+                expected: [-8, 2, 4, 6, 9],
+                time: 1
+            },
+            {
+                array: [1, 2, 5, 3, 4 ],
+                expected: [1, 2, 3, 4, 5],
+                time: 1
+            },
+            {
+                array: [1, 2, 3, 4],
+                expected: [1, 2, 3, 4],
+                time: 1
+            },
+            {
+                array: [3],
+                expected: [3],
+                time: 1
+            },            {
+                array: [ 3, 7, 2, 5, 4, 4 ],
+                expected: [ 2, 3, 4, 4, 5, 7],
+                time: 1
+            }
+        ];
+        testCases.forEach(test => {
+            it(
+                `Array: ${test.array}, expected: ${test.expected}`,
+                async () => {
+                    let onSwap = () => {};
+                    await insertionSort(test.array, onSwap, test.time);
+                    expect(test.array).toEqual(test.expected);
+                }
+            );
+        });
+    }
+)
 
-// test(
-//     'Test Insertion sort exception', 
-//     async() => {
-//         let array = [];
-//         let onSwap = () => {};
-//         try{
-//             await insertionSort(array, onSwap, 1);
-//         } catch(e){
-//             expect(e.message).toBe('Array is empty');
-//         }
-//     }
-// );
+test(
+    'Test Insertion sort exception', 
+    async() => {
+        let array = [];
+        let onSwap = () => {};
+        try{
+            await insertionSort(array, onSwap, 1);
+        } catch(e){
+            expect(e.message).toBe('Array is empty');
+        }
+    }
+);
 
 
-// describe(
-//     'Test Selection Sort',
-//     () => {
-//         const testCases = [
-//             {
-//                 array: [6, 9, 2, -8, 4],
-//                 expected: [-8, 2, 4, 6, 9],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 5, 3, 4 ],
-//                 expected: [1, 2, 3, 4, 5],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 3, 4],
-//                 expected: [1, 2, 3, 4],
-//                 time: 1
-//             },
-//             {
-//                 array: [3],
-//                 expected: [3],
-//                 time: 1
-//             },            {
-//                 array: [ 3, 7, 2, 5, 4, 4 ],
-//                 expected: [ 2, 3, 4, 4, 5, 7],
-//                 time: 1
-//             }
-//         ];
-//         testCases.forEach(test => {
-//             it(
-//                 `Array: ${test.array}, expected: ${test.expected}`,
-//                 async () => {
-//                     let onSwap = () => {};
-//                     await selectionSort(test.array, onSwap, test.time);
-//                     expect(test.array).toEqual(test.expected);
-//                 }
-//             );
-//         });
-//     }
-// )
+describe(
+    'Test Selection Sort',
+    () => {
+        const testCases = [
+            {
+                array: [6, 9, 2, -8, 4],
+                expected: [-8, 2, 4, 6, 9],
+                time: 1
+            },
+            {
+                array: [1, 2, 5, 3, 4 ],
+                expected: [1, 2, 3, 4, 5],
+                time: 1
+            },
+            {
+                array: [1, 2, 3, 4],
+                expected: [1, 2, 3, 4],
+                time: 1
+            },
+            {
+                array: [3],
+                expected: [3],
+                time: 1
+            },            {
+                array: [ 3, 7, 2, 5, 4, 4 ],
+                expected: [ 2, 3, 4, 4, 5, 7],
+                time: 1
+            }
+        ];
+        testCases.forEach(test => {
+            it(
+                `Array: ${test.array}, expected: ${test.expected}`,
+                async () => {
+                    let onSwap = () => {};
+                    await selectionSort(test.array, onSwap, test.time);
+                    expect(test.array).toEqual(test.expected);
+                }
+            );
+        });
+    }
+)
 
-// test(
-//     'Test Selection sort exception', 
-//     async() => {
-//         let array = [];
-//         let onSwap = () => {};
-//         try{
-//             await selectionSort(array, onSwap, 1);
-//         } catch(e){
-//             expect(e.message).toBe('Array is empty');
-//         }
-//     }
-// );
+test(
+    'Test Selection sort exception', 
+    async() => {
+        let array = [];
+        let onSwap = () => {};
+        try{
+            await selectionSort(array, onSwap, 1);
+        } catch(e){
+            expect(e.message).toBe('Array is empty');
+        }
+    }
+);
 
-// describe(
-//     'Test Quick Sort',
-//     () => {
-//         const testCases = [
-//             {
-//                 array: [6, 9, 2, -8, 4],
-//                 expected: [-8, 2, 4, 6, 9],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 5, 3, 4 ],
-//                 expected: [1, 2, 3, 4, 5],
-//                 time: 1
-//             },
-//             {
-//                 array: [1, 2, 3, 4],
-//                 expected: [1, 2, 3, 4],
-//                 time: 1
-//             },
-//             {
-//                 array: [3],
-//                 expected: [3],
-//                 time: 1
-//             },            {
-//                 array: [ 3, 7, 2, 5, 4, 4 ],
-//                 expected: [ 2, 3, 4, 4, 5, 7],
-//                 time: 1
-//             }
-//         ];
-//         testCases.forEach(test => {
-//             it(
-//                 `Array: ${test.array}, expected: ${test.expected}`,
-//                 async () => {
-//                     let onSwap = () => {};
-//                     await quickSort(test.array, onSwap, test.time);
-//                     expect(test.array).toEqual(test.expected);
-//                 }
-//             );
-//         });
-//     }
-// );
+describe(
+    'Test Quick Sort',
+    () => {
+        const testCases = [
+            {
+                array: [6, 9, 2, -8, 4],
+                expected: [-8, 2, 4, 6, 9],
+                time: 1
+            },
+            {
+                array: [1, 2, 5, 3, 4 ],
+                expected: [1, 2, 3, 4, 5],
+                time: 1
+            },
+            {
+                array: [1, 2, 3, 4],
+                expected: [1, 2, 3, 4],
+                time: 1
+            },
+            {
+                array: [3],
+                expected: [3],
+                time: 1
+            },            {
+                array: [ 3, 7, 2, 5, 4, 4 ],
+                expected: [ 2, 3, 4, 4, 5, 7],
+                time: 1
+            }
+        ];
+        testCases.forEach(test => {
+            it(
+                `Array: ${test.array}, expected: ${test.expected}`,
+                async () => {
+                    let onSwap = () => {};
+                    await quickSort(test.array, onSwap, test.time);
+                    expect(test.array).toEqual(test.expected);
+                }
+            );
+        });
+    }
+);
 
-// test(
-//     'Test Quick sort exception',
-//     async() => {
-//         let array = [];
-//         let onSwap = () => {};
-//         try{
-//             await quickSort(array, onSwap, 1);
-//         } catch(e){
-//             expect(e.message).toBe('Array is empty');
-//         }
-//     } 
-// );
+test(
+    'Test Quick sort exception',
+    async() => {
+        let array = [];
+        let onSwap = () => {};
+        try{
+            await quickSort(array, onSwap, 1);
+        } catch(e){
+            expect(e.message).toBe('Array is empty');
+        }
+    } 
+);
 
 describe(
     'Test Merge Sort',
@@ -261,4 +261,17 @@ describe(
         });
     }
 );
+
+test(
+    'Test Merge sort exception',
+    () => {
+        let array = [];
+        try {
+            mergeSort(array);
+        } catch (error) {
+            expect(error.message).toBe('Array is empty');
+        }
+    }
+);
+
 
